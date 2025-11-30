7787c940e6e168858abba6098ce24c5858cf43ff
RinAMFT <mucurkaarina@gmail.com>
2025-11-30 19:04:01 +0200

    Add files via upload

---
+++ Coren_iz_proizved/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Class1
+{
+    public double chislo1;
+    public double chislo2;
+    public double product;
+    public Class1(double chislo1, double chislo2)
+	{
+		this.chislo1 = chislo1;
+        this.chislo2 = chislo2;
+        Resh();
+	}
+    
+    
+
+    public void Output()
+    {
+        product = chislo1 * chislo2;
+        if (product < 0)
+        {
+            Console.WriteLine("Нет действительных корней.");
+        }
+        else
+        {
+            double result = Math.Sqrt(product);
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+
+}
---
+++ Coren_iz_proizved/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coren_iz_proizved
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Число 1: ");
+            //double chislo1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Число 2: ");
+            //double chislo2 = Convert.ToDouble(Console.ReadLine());
+
+            Class1 res = new Class1(1, 2);
+            res.Output();
+            Console.ReadKey();
+        }
+    }
+}
