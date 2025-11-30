0660eb42ce70e58f4b406fce85adb4e927874bb2
RinAMFT <mucurkaarina@gmail.com>
2025-11-30 19:03:41 +0200

    Add files via upload

---
+++ CPU/CPU.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU
+{
+    public class CPU
+    {
+        public string model;
+        public double frequency;
+        public int coreCount;
+        public CPU(string model, double frequency, int coreCount)
+        {
+            this.model = model;
+            this.frequency = frequency;
+            this.coreCount = coreCount;
+        }
+        public double Performance
+        {
+            get
+            {
+                return frequency * coreCount;
+            }
+        }
+        public void Overclock(double percentage)
+        {
+            if (percentage > 0)
+            {
+                this.frequency *= (1 + percentage);
+                Console.WriteLine($"\nНовая частота: {this.frequency:F2} ГГц");
+            }
+            else
+            {
+                Console.WriteLine("Неверный процент разгона. Частота осталась прежней.");
+            }
+        }
+    }
+}
---
+++ CPU/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CPU myCpu = new CPU("Intel Core i7-13700K", 3.40, 16);
+            myCpu.Overclock(0.15);
+            Console.WriteLine($"Новая производительность (Performance): {myCpu.Performance:F2}");
+            Console.ReadKey();
+        }
+    }
+}
