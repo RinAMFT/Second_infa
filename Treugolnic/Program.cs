23f91b98f8fba981c82dea7ba9ea1e3401dc39c0
RinAMFT <mucurkaarina@gmail.com>
2025-11-30 19:02:26 +0200

    Add files via upload

---
+++ Treugolnic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treugolnic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle treug = new Triangle(3, 4, 5);
+            treug.IsValid();
+            treug.GetArea();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Treugolnic/Triangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treugolnic
+{
+    public class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+        public double treug;
+
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+        }
+        public bool IsValid()
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                if (a + b > c)
+                {
+                    if (a + c > b)
+                    {
+                        if (b + c > a)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public double GetPerimeter()
+        {
+            Console.WriteLine(a + b + c);
+            return a + b + c;
+
+        }
+        public void GetArea()
+        {
+            double s = GetPerimeter() / 2; 
+            double areaSquared = s * (s - a) * (s - b) * (s - c);
+            Console.WriteLine(Math.Sqrt(areaSquared));
+        }
+    }
+}
