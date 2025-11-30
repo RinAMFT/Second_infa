dd6d6e516f7d90d04b33591de9323d89915c9757
RinAMFT <mucurkaarina@gmail.com>
2025-11-30 19:03:17 +0200

    Add files via upload

---
+++ Oprosik/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Oprosik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SurveyAnser> survey = new List<SurveyAnser>();
+            survey.Add(new SurveyAnser("Вопрос 1", true, 1));
+            survey.Add(new SurveyAnser("Вопрос 2", false, 2));
+            survey.Add(new SurveyAnser("Вопрос 3", false, 3));
+            int ch = 0;
+            foreach (var item in survey)
+            {
+                if (item.answer == true)
+                {
+                    ch++;
+                }
+                Console.WriteLine($"ответ = {item.answer}");
+                
+            }
+            Console.WriteLine($"количество положительных= {ch}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Oprosik/SurveyAnser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oprosik
+{
+    public class SurveyAnser
+    {
+        public string question { get; set; }
+        public bool answer { get; set; }
+        public int respondentId { get; set; }
+        public int count { get; set; }
+        public SurveyAnser(string question, bool answer, int respondentId)
+        {
+            this.question = question;
+            this.answer = answer;
+            this.respondentId = respondentId;
+        } 
+    }
+}
